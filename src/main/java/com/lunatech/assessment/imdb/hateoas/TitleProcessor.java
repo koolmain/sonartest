57b62d1a2814263260c94da3d99ae2dca8285945
Virendra <koolmain@gmail.com>
2023-01-10 09:28:39 +0530

    Removing printstacktrace

--- src/main/java/com/lunatech/assessment/imdb/hateoas/TitleProcessor.java
+++ src/main/java/com/lunatech/assessment/imdb/hateoas/TitleProcessor.java
@@ -11,16 +11,22 @@ import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.RepresentationModelProcessor;
 import org.springframework.stereotype.Component;
 
+import com.lunatech.assessment.imdb.constants.ImdbI18NConstants;
 import com.lunatech.assessment.imdb.controller.TitlesController;
+import com.lunatech.assessment.imdb.exceptions.ImdbNotFoundException;
 import com.lunatech.assessment.imdb.model.Title;
+import com.lunatech.assessment.imdb.util.ImdbUtils;
 
 @Component
 public class TitleProcessor implements RepresentationModelProcessor<EntityModel<Title>> {
 
-    private RepositoryRestConfiguration configuration; 
+    private static final String URL_SYNTAX_EXCEPTION = "URL Syntax exception";
+	private RepositoryRestConfiguration configuration; 
+	private ImdbUtils utils; 
 
-    public TitleProcessor(RepositoryRestConfiguration configuration){
+    public TitleProcessor(RepositoryRestConfiguration configuration,ImdbUtils utils){
         this.configuration= configuration; 
+		this.utils = utils; 
     }
 
     @Override
@@ -49,7 +55,7 @@ public class TitleProcessor implements RepresentationModelProcessor<EntityModel<
 			newUri = new URI(uri.getScheme(), uri.getUserInfo(), uri.getHost(), //
 					uri.getPort(), basePath + uri.getPath(), uri.getQuery(), uri.getFragment());
 		} catch (URISyntaxException e) {
-			e.printStackTrace();
+			throw new ImdbNotFoundException(URL_SYNTAX_EXCEPTION); 
 		}
 
 		return Link.of(newUri.toString(), link.getRel());
